0dbb6eeceded02c521f12311950f1465500eb3a6
victoriaa040402 <72193031+victoriaa040402@users.noreply.github.com>
2021-04-16 15:06:15 +0100

    Update monster.h

--- monster.h
+++ monster.h
@@ -1,11 +1,11 @@
 #include <iostream>
-
 class Monsters {
 public:
     int enemy_MaxHealth;
     int enemy_CurrentHealth;
     int enemy_DamageDone;
     int enemy_DamageTaken;
+    int enemy_TempCurrentHealth;
 
     void MonsterAttributes() {
         std::cout << "Max Health - " << enemy_MaxHealth << std::endl;
@@ -14,12 +14,24 @@ public:
         std::cout << "Damage Taken - " << enemy_DamageTaken << std::endl;
     }
 };
-int main()
+ int MonsterLevels()
 {
     Monsters enemyLevel1;
     enemyLevel1.enemy_MaxHealth = 100;
     enemyLevel1.enemy_DamageDone = 10;
     enemyLevel1.enemy_DamageTaken = 15;
-    enemyLevel1.enemy_CurrentHealth = enemyLevel1.enemy_MaxHealth - enemyLevel1.enemy_DamageTaken;
-    enemyLevel1.MonsterAttributes();
+    enemyLevel1.enemy_CurrentHealth = 100;
+    Monsters enemyLevel2;
+    enemyLevel2.enemy_MaxHealth = 200;
+    enemyLevel2.enemy_DamageDone = 20;
+    enemyLevel2.enemy_DamageTaken = 25;
+    enemyLevel2.enemy_CurrentHealth = 200;
+    enemyLevel2.MonsterAttributes();
+    Monsters enemyLevel3;
+    enemyLevel3.enemy_MaxHealth = 300;
+    enemyLevel3.enemy_DamageDone = 30;
+    enemyLevel3.enemy_DamageTaken = 30;
+    enemyLevel3.enemy_CurrentHealth = 300;
+    enemyLevel3.MonsterAttributes();
+    return 0;
 }
